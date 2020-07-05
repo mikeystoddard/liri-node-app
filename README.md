156b24c1b84874bf4e6b5c6587565602e1534b42
Michael <mikeystoddard@gmail.com>
2020-07-04 21:15:19 -0700

    completed

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # liri-node-app
+LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+To use this
+- Type "node" for index 0, name of file for index 1, and command (listed below) in index 2, and in index 3, enter artist, band, song, or movie
+Available commands:
+concert-this
+spotify-this-song
+movie-this
+do-what-it-says
+Concert Screenshot
+/Users/user/Documents/uawork/homework/liri-node-app/images/main_full.png
+Spotify Screenshot
+/Users/user/Documents/uawork/homework/liri-node-app/images/main_full (1).png
+Movie Screenshot
+/Users/user/Documents/uawork/homework/liri-node-app/images/main_full (2).png
--- liri.js
+++ liri.js
@@ -5,27 +5,49 @@ var Spotify = require("node-spotify-api");
 var fs = require("fs");
 var axios = require("axios");
 var moment = require("moment");
-// reference in if/else
+var chalk = require("chalk")
+// reference in if/else. Learned slice in class tonight
 var command = process.argv[2]
-var userInput = process.argv.slice(3).join(" ");
+var userInput = process.argv.slice(3).join("");
 
+function startPoint(command, userInput) {
+    if (command === "concert-this") {
+        bandsInTown(userInput)
+    } else if (command === "spotify-this-song") {
+        songs(userInput)
+
+    } else if (command === "movie-this") {
+        movies(userInput)
+
+    } else if (command === "do-what-it-says") {
+        doWhatItSays()
+    } else {
+        console.log("invalid choice");
+        process.exit;
+    }
+}
 // ---------concert-this------------
 function bandsInTown(artist) {
-    // console.log("Artist", artist, "string");
+    console.log("bandsInTown" + artist)
     if (artist === "") {
         artist = "Lady Gaga";
     };
+
     var bandURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     axios.get(bandURL).then(
         function (response) {
             var concerts = response.data;
-            // console.log(response.data);
+            if (!concerts.length) {
+                console.log(chalk.red ("No results found for " + artist));
+                return;
+            }
             for (let i = 0; i < concerts.length; i++) {
-                console.log("Venue: " + concerts[i].venue.name);
-                console.log("Location: " + concerts[i].venue.city + "," + concerts[i].venue.country);
-                console.log("Date: " + moment(concerts[i].datetime).format("MM/DD/YYYY"));
+                console.log(chalk.blue.bgWhite("Venue: " + concerts[i].venue.name));
+                console.log(chalk.blue.bgWhite("Location: " + concerts[i].venue.city + "," + concerts[i].venue.country));
+                console.log(chalk.blue.bgWhite("Date: " + moment(concerts[i].datetime).format("MM/DD/YYYY")));
                 console.log("\n--------------\n");
             }
+            //Data requested from HMWK
         }
     );
 };
@@ -34,12 +56,16 @@ function Artists(artist) {
     return artist.name
 }
 var spotify = new Spotify(keys.spotify);
+
 function songs(song) {
     if (song === "") {
         song = "The Sign";
     };
 
-    spotify.search({ type: 'track', query: song }, function (err, data) {
+    spotify.search({
+        type: 'track',
+        query: song
+    }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
@@ -51,6 +77,7 @@ function songs(song) {
             console.log("Album: " + list[i].album.name);
             console.log("\n--------------\n");
         }
+        //console logs for data points requested from hmwk
     });
 }
 
@@ -73,33 +100,21 @@ function movies(movie) {
             console.log("Plot: " + response.data.Plot);
             console.log("Actors: " + response.data.Actors);
             console.log("\n--------------\n");
-           
+            //data requested from HMWK
         });
 }
+// Do What It Says
+
 function doWhatItSays() {
-    fs.readFile("random.txt", "utf-8", function (err, data) {
-        if (err) {
-            return console.log(err);
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        console.log("data" + data)
+        var dataArray = data.split(",")
+        if (dataArray.length === 2) {
+            startPoint(dataArray[0], dataArray[1])
+        } else if (dataArray.length === 1) {
+            startPoint(dataArray[0])
         }
-        var random = data.split(",");
-        songs(random[1]);
-    });
+    })
 }
 
-if (command === "concert-this") {
-    bandsInTown(userInput)
-}
-
-else if (command === "spotify-this-song") {
-    songs(userInput)
-
-} else if (command === "movie-this") {
-    movies(userInput)
-
-} else if (command === "do-what-it-says") {
-    doWhatItSays()
-} 
-else {
-    console.log("invalid choice");
-    process.exit(0);
-}
+startPoint(command,userInput)
